3ddbc1e367e91fb9a1f55a17690cfaea356b955c
sojib-mondol <mdsojibmandol@gmail.com>
2024-11-23 15:34:45 +0600

    module manifest file

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Opne Phone Api Sync to the CRM',
+    'version': '18.1.0',
+    'summary': 'A brief summary of your module',
+    'description': 'A detailed description of your module, including features and purpose.',
+    'author': 'Sojib Mondol',
+    'website': 'https://metamorphosis.com.bd',
+    'license': 'LGPL-3',  
+    'category': 'Uncategorized', 
+    'depends': ['base', 'web'],  
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+           
+        ],
+        'web.assets_frontend': [
+            
+            
+        ],
+    },
+    # 'images': ['static/description/icon.png'],  
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
