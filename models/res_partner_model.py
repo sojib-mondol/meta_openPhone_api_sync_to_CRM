833b7c1cc9efcfa719484a8839020ffc8940aed2
sojib-mondol <mdsojibmandol@gmail.com>
2024-11-25 18:05:14 +0600

    feat: enhance res.partner with openphone_contact_id field

--- models/res_partner_model.py
+++ models/res_partner_model.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    openphone_contact_id = fields.Char(string="OpenPhone Contact ID")
+    openphone_contact_id = fields.Char(string="OpenPhone Contact ID", readonly=False)
 
