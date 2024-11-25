33b9f2610d33116fb2cfe21051951bea0de9c52f
sojib-mondol <mdsojibmandol@gmail.com>
2024-11-25 18:01:53 +0600

    refactor: no actual changes, testing commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+from . import res_partner_model
 from . import fetch_usets_and_sync_with_odoo_contacts
 from . import post_contact_data_to_openphone
-from . import res_partner_model
+from . import update_contact_data
