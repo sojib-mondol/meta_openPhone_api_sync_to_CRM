6239a19775fa04a56fa8fdc5d93958a45b6a69dc
sojib-mondol <mdsojibmandol@gmail.com>
2024-11-25 18:04:13 +0600

    feat: added fields in contact data

--- models/post_contact_data_to_openphone.py
+++ models/post_contact_data_to_openphone.py
@@ -24,6 +24,8 @@ class ResPartner(models.Model):
             "defaultFields": {
                 "firstName": partner.name.split(" ")[0] if partner.name else "",
                 "lastName": " ".join(partner.name.split(" ")[1:]) if len(partner.name.split(" ")) > 1 else "",
+                "company": partner.company_name if partner.company_name else "",
+                "role": partner.function if partner.function else "",
                 "emails": [{"name": "Work email", "value": partner.email}] if partner.email else [],
                 "phoneNumbers": [{"name": "Work phone", "value": partner.phone}] if partner.phone else [],
             },
@@ -49,8 +51,11 @@ class ResPartner(models.Model):
             if response.status_code == 201:
                 response_data = response.json()
                 contact_id = response_data.get("data", {}).get("id")
-                _logger.info("Successfully created contact in OpenPhone: %s with ID %s", partner.name, contact_id)
-                partner.write({'openphone_contact_id': contact_id})
+                if contact_id:
+                    partner.write({'openphone_contact_id': contact_id})
+                    _logger.info("Saved OpenPhone Contact ID %s for partner %s", contact_id, partner.name)
+                else:
+                    _logger.warning("Contact ID not returned from OpenPhone API for partner %s", partner.name)
             else:
                 # Log and raise an error for non-successful status codes
                 _logger.error("Failed to create contact in OpenPhone: %s", response.text)
