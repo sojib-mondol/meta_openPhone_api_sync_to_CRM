1671103a010119d5f2fc337f532790e944969620
sojib-mondol <mdsojibmandol@gmail.com>
2024-11-26 15:25:05 +0600

    [ADD] init: Import delete_contact_data in __init__.py. Added the `delete_contact_data` import in the `__init__.py` file to ensure the delete contact functionality is included during module initialization.

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from . import res_partner_model
 from . import fetch_usets_and_sync_with_odoo_contacts
 from . import post_contact_data_to_openphone
 from . import update_contact_data
+from . import delete_contact_data
