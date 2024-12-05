04c0eafa58dd6885471ce3700906504d5d204479
sojib-mondol <mdsojibmandol@gmail.com>
2024-12-05 16:05:05 +0600

    get the data from list api and post data into chatter

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from . import fetch_usets_and_sync_with_odoo_contacts
 from . import post_contact_data_to_openphone
 from . import update_contact_data
 from . import delete_contact_data
+from . import list_call_in_chatter
